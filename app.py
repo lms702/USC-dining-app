6b6a8b1a6b557d78909eb707d894788bca13a77a
Scottie Mai <scottmai@usc.edu>
2019-07-07 14:06:08 -0700

    getrekt

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
 	return("<h1>Welcome to the USC Dining App homepage!</h1>")
 
+@app.route('/getrektdhivya')
+def getrekt():
+	return('<h2 style="text-align:center">Guess who just launched their first heroku app</h2>')
+
 if __name__ == "__main__":
   app.run()

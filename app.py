0993472e1db29f5df6cc5570b7f9475484968762
Scottie Mai <scottmai@usc.edu>
2019-07-08 07:26:36 -0700

    took out dhivya

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
 	return("<h1>Welcome to the USC Dining App homepage!</h1>")
 
-@app.route('/getrektdhivya')
+@app.route('/getrekt')
 def getrekt():
 	return('<h2 style="text-align:center">Guess who just launched their first heroku app</h2>')
 
